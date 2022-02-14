02fee2fa21f6285e91fd849d3ede4f0f1be44dd6
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-14 14:26:04 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -18,15 +18,18 @@ namespace ConsoleApp1
             Console.WriteLine("What course are you on?");
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer true or false.");
             string helpNeeded = Console.ReadLine();
+            bool needsHelp = false;
+            if (helpNeeded == "true")
+                needsHelp = true;
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
             string positives = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hoursStudied = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
