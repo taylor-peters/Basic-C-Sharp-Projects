815d17487207af6bdcb22217774df90404b16977
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-21 12:38:56 -0800

    Method Class

---
+++ FourthCalc/FourthCalc/Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCalc
+{
+    class Number
+    {
+
+        public int numOne { get; set; }
+        public int numTwo { get; set; }
+
+        // Creates optional parameter for second integer
+        public static void Add(int numOne, int numTwo)
+        {
+            // Performs math operation on first integer - never displayed
+            int doubleOne = numOne + numOne;
+            Console.WriteLine("Writing the second integer");
+            Console.WriteLine(numTwo);
+        }
+    }
+}
---
+++ FourthCalc/FourthCalc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace FourthCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Number number = new Number();
+
+            Console.WriteLine("Choose two numbers.");
+            number.numOne = Convert.ToInt32(Console.ReadLine()); 
+            number.numTwo = Convert.ToInt32(Console.ReadLine());
+            
+            // Passes both arguments to Add method
+            Number.Add(numOne: number.numOne, numTwo: number.numTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
