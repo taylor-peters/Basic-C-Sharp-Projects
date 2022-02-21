21d3fbd95a78c38e8891951d26f757978035d3e9
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-21 10:48:14 -0800

    Main Method Submission Assignment

---
+++ SecondCalc/SecondCalc/Number.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondCalc
+{
+    class Number
+    {
+        public Number()
+        {
+        }
+
+        public int numberValue { get; set; }
+        public decimal decimalValue { get; set; }
+        public string stringValue { get; set; }
+
+        public static int Double(int numberValue)
+        {
+            // Converts user input to integer
+            numberValue = Convert.ToInt32(Console.ReadLine());
+            numberValue = numberValue + numberValue;
+            return numberValue;
+        }
+
+        public static int Double(decimal decimalValue)
+        {
+            // Converts user input to decimal
+            decimalValue = Convert.ToDecimal(Console.ReadLine());
+            decimalValue = decimalValue * 5;
+            return Convert.ToInt32(decimalValue);
+        }
+
+        public static int Double(string stringValue)
+        {
+            // Takes user input
+            stringValue = Console.ReadLine();
+            try
+            { 
+                // Checks if calculation is possible
+                int temp = Convert.ToInt32(stringValue);
+                temp *= temp;
+                stringValue = Convert.ToString(temp);
+            } catch (FormatException)
+            {
+                Console.WriteLine("{0} is an incalculable input", stringValue);
+                Console.ReadLine();
+            }
+            return Convert.ToInt32(stringValue);
+        }
+    }
+}
---
+++ SecondCalc/SecondCalc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Number number = new Number();
+            Console.WriteLine("Welcome to my second 'calculator'");
+
+            Console.WriteLine("Type an integer");            
+            Console.WriteLine(Number.Double(number.numberValue));
+            Console.WriteLine("Integer Operation: Number Was Doubled\n");
+            
+            Console.WriteLine("Type an decimal");
+            Console.WriteLine(Number.Double(number.decimalValue));
+            Console.WriteLine("Decimal Operation: Number Multiplied by 5\n");        
+
+            Console.WriteLine("Type an string");
+            Console.WriteLine(Number.Double(number.stringValue));
+            Console.WriteLine("String Operation: Number Squared\n");
+            
+            Console.ReadLine();
+        }
+    }
+}
