50faa7f121d97f6b4ea0a3fe46521fe873f1da9b
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-21 09:04:42 -0800

    Calling Methods Assignment

---
+++ FirstCalc/FirstCalc/Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCalc
+{
+    public class Number
+    {
+        public Number()
+        {
+            numberValue = Convert.ToInt32(Console.ReadLine());
+        }
+        public int numberValue { get; set; }
+
+        public static int Double(int numberValue)
+        {
+            numberValue = numberValue * 2;
+            return numberValue;
+        }
+
+        public static int Square(int numberValue)
+        {
+            numberValue *= numberValue;
+            return numberValue;
+        }
+
+        public static int Cube(int numberValue)
+        {
+            numberValue = (numberValue * numberValue * numberValue);
+            return numberValue;
+        }
+
+    }
+}
---
+++ FirstCalc/FirstCalc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my 'calculator'");
+            Console.WriteLine("Pick a number");
+            Number number = new Number();
+
+            Console.WriteLine("Doubling the number");
+            Console.WriteLine(Number.Double(number.numberValue));
+
+            Console.WriteLine("Squaring the number");
+            Console.WriteLine(Number.Square(number.numberValue));
+
+            Console.WriteLine("Cubing the number");
+            Console.WriteLine(Number.Cube(number.numberValue));
+
+
+
+            //if (operation.ToLower() == "double")
+            //{
+            //    number.numberValue = Number.Double(number.numberValue);
+            //}
+            //else if (operation.ToLower() == "square")
+            //{
+            //    number.numberValue = Number.Square(number.numberValue);
+            //}
+            //else if (operation.ToLower() == "cube")
+            //{
+            //    number.numberValue = Number.Cube(number.numberValue);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Incorrect request, please try again");
+            //    Console.ReadLine();
+            //}
+            //Console.WriteLine("Your answer is: " + number.numberValue);
+            Console.ReadLine();
+
+        }
+    }
+}
