be956e34a9e235a6febfb210abb284b4ea6491da
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-22 10:45:09 -0800

    Abstract Class Submission

---
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public class Employee : Person
+    { 
+        public int iD { get; set; }
+        
+        // Virtual tag allows us to overwrite empty method from superclass
+        public virtual void SayName()
+        {
+            string fullName = (firstName + " " + lastName);
+            Console.WriteLine("Name: " + fullName);
+        }
+    }
+}
---
+++ AbstractClassSubmission/AbstractClassSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee();
+
+            employeeOne.firstName = "Sample";
+            employeeOne.lastName = "Student";        
+
+            // Calls modified, via the virtual tag, inherited method from abstract superclass
+            employeeOne.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
