33add5cd5ec71ed25e54168c20e36ed16693e6fb
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-21 23:34:11 -0800

    Inheritance Submission

---
+++ NameSayer/NameSayer/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NameSayer
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public void SayName()
+        {
+            // Concatenates names
+            string fullName = (firstName + " " + lastName);
+            Console.WriteLine("Name: " + fullName);
+        }
+
+    }
+
+}
---
+++ NameSayer/NameSayer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Taylor";
+            person.lastName = "Peters";
+            person.SayName();
+            Console.ReadLine();
+
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.id = 12345;
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
