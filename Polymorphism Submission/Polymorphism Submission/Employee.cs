229147b3dd9c373aa19245c6cf7d162c664af2f9
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-22 11:09:10 -0800

    Polymorphism & Interfaces

---
+++ Polymorphism Submission/Polymorphism Submission/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Submission
+{
+    class Employee : Person, IQuittable
+    {
+        bool isActive = true;
+
+        // Virtual tag allows us to overwrite empty method from superclass
+        public virtual void SayName()
+        {
+            string fullName = (firstName + " " + lastName);
+            Console.WriteLine("Name: " + fullName);
+        }
+        public void Quit()
+        {
+            isActive = false;
+        }
+      
+    }
+}
---
+++ Polymorphism Submission/Polymorphism Submission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee();
+
+            employeeOne.firstName = "Sample";
+            employeeOne.lastName = "Student";
+
+            // Calls modified, via the virtual tag, inherited method from abstract superclass
+            employeeOne.SayName();
+            Console.ReadLine();
+
+            // Polymorphism
+            IQuittable quitter = new Employee();
+            quitter.Quit();
+        }
+    }
+}
