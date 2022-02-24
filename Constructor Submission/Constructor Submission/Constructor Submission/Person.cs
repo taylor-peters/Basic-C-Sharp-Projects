b5d65b083302002e60c75855f4b4763c2f9d47e6
Taylor Peters <93218689+taylor-peters@users.noreply.github.com>
2022-02-23 22:45:49 -0800

    Constructor Submission

---
+++ Constructor Submission/Constructor Submission/Constructor Submission/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Person
+    {
+       
+        // Constructor chained
+        public Person(string firstName) : this(firstName, "Doe")
+        {
+        }
+        public Person(string firstName, string lastName)
+        {
+            fName = firstName;
+            lName = lastName;
+        }
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+}
---
+++ Constructor Submission/Constructor Submission/Constructor Submission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int ultimateAnswer = 42;
+            var name = "Douglas";
+
+            // Creates two new objects, one with two parameters, one with one
+            Person personOne = new Person("Taylor", "Peters");
+            Person personTwo = new Person("John");
+            {
+                // Calls 'missing' parameter
+                Console.WriteLine(personTwo.lName);
+                Console.ReadLine();
+            };
+        }
+    }
+}
